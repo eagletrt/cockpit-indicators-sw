5a4167b4a0b380a0a2a8b6fb1875cf8927f74b7e
Bridiro <ale.bridi15@gmail.com>
2025-05-08 21:05:31 +0200

    feat: labels for led pins

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -49,13 +49,13 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
-                          |GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, TS_OFF_Pin|IMD_Pin|AMS_Pin|TSAL_RED_Pin
+                          |TSAL_GREEN_Pin|TSAL_BLUE_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PA2 PA3 PA4 PA5
-                           PA6 PA7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
-                          |GPIO_PIN_6|GPIO_PIN_7;
+  /*Configure GPIO pins : TS_OFF_Pin IMD_Pin AMS_Pin TSAL_RED_Pin
+                           TSAL_GREEN_Pin TSAL_BLUE_Pin */
+  GPIO_InitStruct.Pin = TS_OFF_Pin|IMD_Pin|AMS_Pin|TSAL_RED_Pin
+                          |TSAL_GREEN_Pin|TSAL_BLUE_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
