20c50b3344a079e356b162bb7088db68e6a507e3
ale-giu <alexjusti67n@gmail.com>
2025-11-17 18:51:35 +0100

    feat: added indicators module

---
+++ Core/Inc/indicators.h
@@ -0,0 +1,48 @@
+#ifndef INDICATORS_H
+#define INDICATORS_H
+
+#include <stdbool.h>
+#include <main.h>
+
+#define MISSION_PINS 3
+#define MAX_MISSION_LEDS ((1 << MISSION_PINS) - 1)
+
+typedef struct {
+    bool AMS;           //!< Accumulator Management System indicator
+    bool IMD;           //!< Insulation Monitoring Device indicator
+    bool TS_OFF;        //!< Traction System Off indicator
+    uint8_t mission_id; //!< Selected mission profile indicator (0-7)
+    bool changed_state; //!< Flag to indicate if the state has changed
+} LEDState;
+
+typedef struct {
+    GPIO_TypeDef *AMS_port; //!< GPIO port for the AMS indicators
+    uint16_t AMS_pin;       //!< GPIO pin for AMS indicator
+
+    GPIO_TypeDef *IMD_port; //!< GPIO port for the IMD indicators
+    uint16_t IMD_pin;       //!< GPIO pin for IMD indicator
+
+    GPIO_TypeDef *TS_OFF_port; //!< GPIO port for TS_OFF indicator
+    uint16_t TS_OFF_pin;       //!< GPIO pin for TS_OFF indicator
+
+    GPIO_TypeDef *MISSION_ports[MISSION_PINS]; //!< GPIO port for mission profile indicators
+    uint16_t MISSION_pins[MISSION_PINS];       //!< GPIO pins for mission profile indicators
+} LEDPins;
+
+/*!
+ * \brief Update the indicator LEDs based on the current state.
+ * \param state The struct containing the current state of the LED indicators.
+ * \param pins The struct containing the pins to the LED indicators.
+ */
+void update_indicators(const LEDState *state, const LEDPins *pins);
+
+/*! 
+ * \brief Update the mission profile LEDs based on the selected profile.
+ * \param state The struct containing the current state of the LED indicators.
+ * \param pins The struct containing the pins to the LED indicators.
+ * 
+ * \return True if the mission value is valid, else false
+ */
+bool update_mission(const LEDState *state, const LEDPins *pins);
+
+#endif // INDICATORS_H
---
+++ Core/Src/indicators.c
@@ -0,0 +1,36 @@
+#include "indicators.h"
+
+void update_indicators(const LEDState *state, const LEDPins *pins) {
+
+    if (state == NULL || pins == NULL) {
+        return;
+    }
+
+    HAL_GPIO_WritePin(pins->AMS_port, pins->AMS_pin, state->AMS ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(pins->IMD_port, pins->IMD_pin, state->IMD ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(pins->TS_OFF_port, pins->TS_OFF_pin, state->TS_OFF ? GPIO_PIN_SET : GPIO_PIN_RESET);
+}
+
+bool update_mission(const LEDState *state, const LEDPins *pins) {
+
+    if (state == NULL || pins == NULL) {
+        return false;
+    }
+
+    // The pins will be connected to a binary selector:
+    // Mission 0 -> all off
+    // Mission 1 -> LED 0 on
+    // Mission 2 -> LED 1 on ecc...
+
+    uint8_t mission = state->mission_id;
+
+    if (mission > MAX_MISSION_LEDS) {
+        return false;
+    }
+
+    for (int i = 0; i < MISSION_PINS; i++) {
+        HAL_GPIO_WritePin(pins->MISSION_ports[i], pins->MISSION_pins[i], (mission >> i) & 0x01 ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    }
+
+    return true;
+}
