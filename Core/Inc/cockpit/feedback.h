0a89391b99e39c1f3e4b0df728f02c039a269a14
Alessandro Giustina <alexjusti67n@gmail.com>
2025-12-03 13:26:58 +0100

    fix: debugging hooks

--- Core/Inc/cockpit/feedback.h
+++ Core/Inc/cockpit/feedback.h
@@ -45,9 +45,19 @@ typedef enum FeedbackState (*read_feedback)(enum FeedbackName feedback);
  * \brief Struct that handles all relevant feedback information
  */
 struct FeedbackHandler {
-
     read_feedback read_fb;                                          //!< Function to read feedback line state
+    
+    
+    
+    
     volatile enum FeedbackState fb_line_state[FEEDBACK_NAME_COUNT]; //!< Feedback button state array
+    
+    
+    
+    
+    
+    
+    
     volatile bool initialized;                                      //!< Indicates if the feedback handler has been initialized
 };
 
