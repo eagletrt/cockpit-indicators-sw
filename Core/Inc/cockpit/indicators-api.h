5d6a97f9167e1c8c76418b2001939d03252f7697
MattWaX <mattwexx2929@gmail.com>
2026-04-26 12:37:29 +0200

    refactor: splited `indicators-api.h`

--- Core/Inc/cockpit/indicators-api.h
+++ Core/Inc/cockpit/indicators-api.h
@@ -1,35 +1,7 @@
 #ifndef INDICATORS_API_H
 #define INDICATORS_API_H
 
-#include <stdbool.h>
-#include <stdint.h>
-
-/*!
- * \brief Enum for identifying indicators
- */
-enum IndicatorsName {
-    INDICATORS_NAME_AMS,    //!< AMS Indicator
-    INDICATORS_NAME_IMD,    //!< IMD Indicator
-    INDICATORS_NAME_TS_OFF, //!< TS_OFF Indicator
-    INDICATORS_NAME_TSAL,   //!< TSAL Indicator
-    INDICATORS_NAME_COUNT   //!< Total number of indicators
-};
-
-/*!
- * \brief Enum with possible return codes for indicators module
- */
-enum IndicatorsReturnCode {
-    INDICATORS_RC_OK = 0,
-    INDICATORS_RC_ERROR,
-};
-
-/*!
- * \brief Struct that handles all relevant indicator information
- */
-struct IndicatorsHandler {
-    float luminosity;                  //!< PWM value for all indicators (0f-1f)
-    bool state[INDICATORS_NAME_COUNT]; //!< State of each indicator (on/off)
-};
+#include "indicators.h"
 
 /*!
  * \brief Initialize the indicator module and sets them to OFF state.
---
+++ Core/Inc/cockpit/indicators.h
@@ -0,0 +1,35 @@
+#ifndef INDICATORS_H
+#define INDICATORS_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/*!
+ * \brief Enum for identifying indicators
+ */
+enum IndicatorsName {
+    INDICATORS_NAME_AMS,    //!< AMS Indicator
+    INDICATORS_NAME_IMD,    //!< IMD Indicator
+    INDICATORS_NAME_TS_OFF, //!< TS_OFF Indicator
+    INDICATORS_NAME_TSAL,   //!< TSAL Indicator
+    INDICATORS_NAME_COUNT   //!< Total number of indicators
+};
+
+/*!
+ * \brief Enum with possible return codes for indicators module
+ */
+enum IndicatorsReturnCode {
+    INDICATORS_RC_OK = 0,
+    INDICATORS_RC_ERROR,
+};
+
+/*!
+ * \brief Struct that handles all relevant indicator information
+ */
+struct IndicatorsHandler {
+    float luminosity;                  //!< PWM value for all indicators (0f-1f)
+    bool state[INDICATORS_NAME_COUNT]; //!< State of each indicator (on/off)
+};
+
+
+#endif // INDICATORS_H
