dcc777dfa6445bb30837b0c58d7fffe706dcebe0
MattWaX <mattwexx2929@gmail.com>
2026-04-27 16:12:40 +0200

    fix: test `indicators_api_get_indicator`

--- test/test_indicators_api/test_indicators_api.c
+++ test/test_indicators_api/test_indicators_api.c
@@ -37,7 +37,7 @@ void test_indicators_api_initialization(void) {
 /*! \} */
 
 /*!
- * \defgroup test_indicators_api_set
+ * \defgroup test_indicators_api_set_indicator
  * \{
  */
 
@@ -61,6 +61,31 @@ void test_indicators_api_set_on_indicators_invalid_state(void) {
         INDICATORS_RC_ERROR,
         indicators_set_rc,
         "Return code should be INDICATORS_RC_ERROR");
+}
+
+/*! \} */
+
+/*!
+ * \defgroup test_indicators_api_get_indicator
+ * \{
+ */
+
+void test_indicators_api_get_indicator_valid_indicator(void) {
+    indicators_api_handler.state[INDICATORS_NAME_IMD] = true;
+
+    bool indicator_state = indicators_api_get_indicator(INDICATORS_NAME_IMD);
+
+    TEST_ASSERT_TRUE_MESSAGE(
+        indicators_api_handler.state[INDICATORS_NAME_IMD],
+        "Indicator state should be true");
+}
+
+void test_indicators_api_get_indicator_invalid_indicator(void) {
+    bool indicator_state = indicators_api_get_indicator(INDICATORS_NAME_COUNT);
+
+    TEST_ASSERT_FALSE_MESSAGE(
+        indicators_api_handler.state[INDICATORS_NAME_IMD],
+        "An indicator that is not valid should return false");
 }
 
 /*! \} */
@@ -102,6 +127,8 @@ int main() {
     RUN_TEST(test_indicators_api_initialization);
     RUN_TEST(test_indicators_api_set_on_indicators_valid_state);
     RUN_TEST(test_indicators_api_set_on_indicators_invalid_state);
+    RUN_TEST(test_indicators_api_get_indicator_valid_indicator);
+    RUN_TEST(test_indicators_api_get_indicator_invalid_indicator);
     RUN_TEST(test_indicators_api_set_luminosity_in_range_value);
     RUN_TEST(test_indicators_api_set_luminosity_out_of_range_value_high);
     RUN_TEST(test_indicators_api_set_luminosity_out_of_range_value_low);
