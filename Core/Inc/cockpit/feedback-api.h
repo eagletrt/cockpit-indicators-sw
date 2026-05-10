f4ac665f87dc93b33ddee41daaaab50f49b6baa8
MattWaX <mattwexx2929@gmail.com>
2026-05-10 11:22:17 +0200

    chore: simplified documentation

--- Core/Inc/cockpit/feedback-api.h
+++ Core/Inc/cockpit/feedback-api.h
@@ -4,15 +4,14 @@
 #include "feedback.h"
 
 /*!
- * \brief  Initialize feedback module
+ * \brief  Initialize feedback-api module to `FEEDBACK_STATUS_ERROR`
  */
 enum FeedbackReturnCode feedback_api_init();
 
 /*!
  * \brief  Get the current state of a feedback line
- * \param  feedback Enum indicating which feedback line to read
- * \return FeedbackState indicating if any button is pressed (FEEDBACK_STATUS_HIGH, FEEDBACK_STATUS_LOW, FEEDBACK_STATUS_ERROR)
- */
+ * \return state of the `feedback`
+ * */
 enum FeedbackState feedback_api_get_state(enum FeedbackName feedback);
 
 /*!
