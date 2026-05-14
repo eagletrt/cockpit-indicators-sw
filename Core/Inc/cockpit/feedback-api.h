dfb4209e6c91b37253eb96bb186a892424333748
MattWaX <mattwexx2929@gmail.com>
2026-05-14 13:01:17 +0200

    chore: doxygen comments

--- Core/Inc/cockpit/feedback-api.h
+++ Core/Inc/cockpit/feedback-api.h
@@ -5,19 +5,29 @@
 
 /*!
  * \brief  Initialize feedback-api module to `FEEDBACK_STATUS_ERROR`
+ *
+ * \retval FEEDBACK_RC_OK if successful
+ * \retval FEEDBACK_RC_ERROR if fail
  */
 enum FeedbackReturnCode feedback_api_init(void);
 
 /*!
  * \brief  Get the current state of a feedback line
- * \param  `enum FeedbackName feedback`
+ *
+ * \param feedback The feedback to query
+ *
+ * \retval enum FeedbackState of \p feedback
  * */
 enum FeedbackState feedback_api_get_state(enum FeedbackName feedback);
 
 /*!
  * \brief  Set the current state of a feedback line
- * \param  `enum FeedbackName feedback`
- * \param  `enum FeedbackState state`
+ *
+ * \param feedback The feedback to be set
+ * \param state The state to set \p feedback to
+ *
+ * \retval FEEDBACK_RC_OK if successful
+ * \retval FEEDBACK_RC_ERROR if \p feedback or \p state are out of bounds
  */
 enum FeedbackReturnCode feedback_api_set_state(enum FeedbackName feedback, enum FeedbackState state);
 
