edf3e71af58f13ab837733e410a6262f94ce3fe3
MattWaX <mattwexx2929@gmail.com>
2026-06-09 10:50:18 +0200

    chore: init the can module

---
+++ Core/Inc/cockpit/can-api.h
@@ -0,0 +1,10 @@
+#ifndef CAN_API_H
+#define CAN_API_H
+
+#include "pal-api.h"
+
+// TODO? init function
+
+// TODO: receive and transmit with pal functions
+
+#endif // CAN_API_H
---
+++ Core/Inc/cockpit/can.h
@@ -0,0 +1,6 @@
+#ifndef CAN_H
+#define CAN_H
+
+// TODO: Module handler 
+
+#endif // CAN_H
