e71af190a42cd04ac4e25a08b48d6f362224f58d
MattWaX <mattwexx2929@gmail.com>
2026-06-22 13:11:21 +0200

    fix: basic skeleton of the code

--- Core/Src/cockpit/comunication-api.c
+++ Core/Src/cockpit/comunication-api.c
@@ -2,10 +2,17 @@
 #include "comunication-api.h"
 
 #include "pal.h"
+#include "pal-api.h"
 #include "eagletrt.h"
 // #include "eagletrt-api.h"
 #include "arena-allocator.h"
 #include "arena-allocator-api.h"
+
+#define RX_CAPACITY (8 * 64U)
+#define TX_CAPACITY (8 * 64U)
+#define MAX_MSG_SIZE (8U)
+
+static void *tmp_buffer; // TODO: use can stuff when available
 
 EAGLETRT_STATIC struct PalHandler pal_handler;
 EAGLETRT_STATIC struct ArenaAllocatorHandler arena;
@@ -16,11 +23,11 @@ enum ComunicationReturnCode comunication_api_init(void) {
 
     if (pal_api_init(
             &pal_handler,
-            10U,
-            10U,
-            64U,
-            NULL,
-            NULL,
+            RX_CAPACITY,
+            TX_CAPACITY,
+            MAX_MSG_SIZE,
+            NULL, // TODO: put callback
+            NULL, // TODO: put callback
             NULL,
             NULL,
             &arena) != PAL_RC_OK) {
@@ -32,5 +39,25 @@ enum ComunicationReturnCode comunication_api_init(void) {
 
 enum ComunicationReturnCode comunication_api_deinit(void) {
     arena_allocator_api_free(&arena);
+    return COMUNICATION_RC_OK;
+}
+
+// To use inside the receive can callback
+enum ComunicationReturnCode comunication_api_read_from_queue(uint8_t *out_message, uint32_t size) {
+    pal_api_add_to_rx_queue(&pal_handler, out_message, size);
+    return COMUNICATION_RC_OK;
+}
+
+enum ComunicationReturnCode comunication_api_process_rx_queue(void) {
+    if (pal_api_process_rx(&pal_handler, tmp_buffer) != PAL_RC_OK)
+        return COMUNICATION_RC_ERROR;
+
+    return COMUNICATION_RC_OK;
+}
+
+enum ComunicationReturnCode comunication_api_write_to_queue(uint8_t *in_message, uint32_t size) {
+    return COMUNICATION_RC_OK;
+}
+enum ComunicationReturnCode comunication_api_process_tx_queue(void) {
     return COMUNICATION_RC_OK;
 }
