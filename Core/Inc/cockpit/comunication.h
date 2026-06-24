126a9dda5c10b4097216cdaae5cb6363c27c6d99
MattWaX <mattwexx2929@gmail.com>
2026-06-24 13:23:40 +0200

    fix: comunication handler struct

--- Core/Inc/cockpit/comunication.h
+++ Core/Inc/cockpit/comunication.h
@@ -1,11 +1,18 @@
 #ifndef COMUNICATION_H
 #define COMUNICATION_H
 
+#include "pal.h"
+#include "arena-allocator.h"
+
 enum ComunicationReturnCode {
     COMUNICATION_RC_OK = 0,
     COMUNICATION_RC_ERROR,
 };
 
-// TODO: Module handler
+struct ComunicationHandler {
+    struct PalHandler pal_handler;
+    struct ArenaAllocatorHandler arena;
+    void *tmp_rx_buffer; // TODO: use can stuff when available
+};
 
 #endif // COMUNICATION_H
--- Core/Src/cockpit/comunication-api.c
+++ Core/Src/cockpit/comunication-api.c
@@ -12,15 +12,16 @@
 #define TX_CAPACITY (8 * 64U)
 #define MAX_MSG_SIZE (8U)
 
-static void *tmp_rx_buffer; // TODO: use can stuff when available
-// static void *tmp_tx_buffer; // TODO: use can stuff when available
+EAGLETRT_STATIC struct ComunicationHandler comunication_api_handler;
 
-EAGLETRT_STATIC struct PalHandler pal_handler;
-EAGLETRT_STATIC struct ArenaAllocatorHandler arena;
-
-// TODO: Manage errors
+// TODO: Manage errors more decently
 enum ComunicationReturnCode comunication_api_init(void) {
+    struct PalHandler pal_handler;
+    struct ArenaAllocatorHandler arena;
+
     arena_allocator_api_init(&arena);
+
+    comunication_api_handler.arena = arena;
 
     if (pal_api_init(
             &pal_handler,
@@ -35,38 +36,40 @@ enum ComunicationReturnCode comunication_api_init(void) {
         return COMUNICATION_RC_ERROR;
     }
 
+    comunication_api_handler.pal_handler = pal_handler;
+
     return COMUNICATION_RC_OK;
 }
 
 enum ComunicationReturnCode comunication_api_deinit(void) {
-    arena_allocator_api_free(&arena);
+    arena_allocator_api_free(&comunication_api_handler.arena);
     return COMUNICATION_RC_OK;
 }
 
 // To use inside the receive can callback
 enum ComunicationReturnCode comunication_api_add_to_rx_queue(uint8_t *out_message, uint32_t size) {
-    if (pal_api_add_to_rx_queue(&pal_handler, out_message, size) != PAL_RC_OK) {
+    if (pal_api_add_to_rx_queue(&comunication_api_handler.pal_handler, out_message, size) != PAL_RC_OK) {
         return COMUNICATION_RC_ERROR;
     }
     return COMUNICATION_RC_OK;
 }
 
 enum ComunicationReturnCode comunication_api_process_rx_queue(void) {
-    if (pal_api_process_rx(&pal_handler, tmp_rx_buffer) != PAL_RC_OK) {
+    if (pal_api_process_rx(&comunication_api_handler.pal_handler, comunication_api_handler.tmp_rx_buffer) != PAL_RC_OK) {
         return COMUNICATION_RC_ERROR;
     }
     return COMUNICATION_RC_OK;
 }
 
 enum ComunicationReturnCode comunication_api_add_to_tx_queue(uint8_t *in_message, uint32_t size) {
-    if (pal_api_add_to_tx_queue(&pal_handler, in_message, size) != PAL_RC_OK) {
+    if (pal_api_add_to_tx_queue(&comunication_api_handler.pal_handler, in_message, size) != PAL_RC_OK) {
         return COMUNICATION_RC_ERROR;
     }
     return COMUNICATION_RC_OK;
 }
 
 enum ComunicationReturnCode comunication_api_process_tx_queue(void) {
-    if (pal_api_process_tx(&pal_handler) != PAL_RC_OK) {
+    if (pal_api_process_tx(&comunication_api_handler.pal_handler) != PAL_RC_OK) {
         return COMUNICATION_RC_ERROR;
     }
     return COMUNICATION_RC_OK;
