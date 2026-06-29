a02dfe8a4e0047906f8cff9a6a9de6d9bf565ae9
MattWaX <mattwexx2929@gmail.com>
2026-06-29 18:20:01 +0200

    fix: use a struct to handle comunication messages

--- Core/Inc/cockpit/comunication-api.h
+++ Core/Inc/cockpit/comunication-api.h
@@ -7,13 +7,14 @@
 
 #include "stdint.h"
 
-enum ComunicationReturnCode comunication_api_init(void);
+enum ComunicationReturnCode comunication_api_init(
+    pal_deserialize_callback pal_deserialize, pal_send_callback pal_send);
 enum ComunicationReturnCode comunication_api_deinit(void);
 
 // TODO: receive and transmit with pal functions
-enum ComunicationReturnCode comunication_api_add_to_rx_queue(uint8_t *out_message, uint32_t size);
-enum ComunicationReturnCode comunication_api_add_to_tx_queue(uint8_t *in_message, uint32_t size);
+enum ComunicationReturnCode comunication_api_add_to_rx_queue(struct ComunicationFrame *frame);
 enum ComunicationReturnCode comunication_api_process_rx_queue(void);
+enum ComunicationReturnCode comunication_api_add_to_tx_queue(struct ComunicationFrame *frame);
 enum ComunicationReturnCode comunication_api_process_tx_queue(void);
 
 #endif // COMUNICATION_API_H
--- Core/Inc/cockpit/comunication.h
+++ Core/Inc/cockpit/comunication.h
@@ -4,15 +4,26 @@
 #include "pal.h"
 #include "arena-allocator.h"
 
+#define RX_CAPACITY (8 * 64U)
+#define TX_CAPACITY (8 * 64U)
+#define MAX_MSG_SIZE (8U)
+
 enum ComunicationReturnCode {
     COMUNICATION_RC_OK = 0,
+    COMUNICATION_RC_NULL_POINTER,
     COMUNICATION_RC_ERROR,
 };
 
 struct ComunicationHandler {
     struct PalHandler pal_handler;
     struct ArenaAllocatorHandler arena;
-    void *tmp_rx_buffer; // TODO: use can stuff when available
+    void *tmp_rx_buffer; // TODO: maybe use callback?
+};
+
+struct [[gnu::packed]] ComunicationFrame {
+    uint32_t id;                /*!< CAN identifier (Standard 11-bit or Extended 29-bit) */
+    uint8_t length;             /*!< Count of valid data bytes within the array (0..8) */
+    uint8_t data[MAX_MSG_SIZE]; /*!< Raw payload byte buffer */
 };
 
 #endif // COMUNICATION_H
